8f3598e26cd3f6858ca03a38c2971dfbb91c90e3
RolandoDrRobot <rolando.arguello.email@gmail.com>
2024-11-05 21:30:50 -0600

    Style cartridge controller button

--- client/src/components/CartridgeController/ControllerConnectButton.tsx
+++ client/src/components/CartridgeController/ControllerConnectButton.tsx
@@ -1,23 +1,30 @@
 import { useConnect, useAccount } from "@starknet-react/core";
+import dojo from '../../img/dojo-icon.svg'
 
 const ControllerConnectButton = () => {
   const { connect, connectors } = useConnect();
   const { address, status } = useAccount();
 
-  if (status === "connected" && address) {
-    return <div>Connected: {address}</div>;
-  }
-
   return (
     <div>
       {connectors.map((connector) => (
-        <button
-          key={connector.id}
-          onClick={() => connect({ connector })}
-          className="connect-btn"
-        >
-          Connect with Controller
-        </button>
+        status === "connected" ? (
+          <button
+            key={connector.id}
+            className="connect-btn"
+          >
+            Account ...{address?.slice(-6)}
+          </button>
+        ) : (
+          <button
+            key={connector.id}
+            onClick={() => connect({ connector })}
+            className="connect-btn"
+          >
+            Connect
+            <img src={dojo} alt="starknet" />
+          </button>
+        )
       ))}
     </div>
   );
