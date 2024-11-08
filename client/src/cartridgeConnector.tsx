3779b88bc5e592575ca5a39d983d4190f5e64d64
RolandoDrRobot <rolando.arguello.email@gmail.com>
2024-11-07 23:41:12 -0600

    contract

--- client/src/cartridgeConnector.tsx
+++ client/src/cartridgeConnector.tsx
@@ -1,42 +1,45 @@
 import ControllerConnector from "@cartridge/connector";
+import { Policy } from "@cartridge/controller";
 import { Connector } from "@starknet-react/core";
+
+const policies: Policy[] = [
+  {
+    target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
+    method: "spawn",
+  },
+  {
+    target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
+    method: "decrease_stats",
+  },
+  {
+    target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
+    method: "feed",
+  },
+  {
+    target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
+    method: "sleep",
+  },
+  {
+    target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
+    method: "awake",
+  },
+  {
+    target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
+    method: "play",
+  },
+  {
+    target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
+    method: "clean",
+  },
+  {
+    target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
+    method: "revive",
+  },
+]
 
 // Configuración básica del conector
 const cartridgeConnector = new ControllerConnector({
-  policies: [
-    {
-      target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
-      method: "spawn",
-    },
-    {
-      target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
-      method: "decrease_stats",
-    },
-    {
-      target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
-      method: "feed",
-    },
-    {
-      target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
-      method: "sleep",
-    },
-    {
-      target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
-      method: "awake",
-    },
-    {
-      target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
-      method: "play",
-    },
-    {
-      target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
-      method: "clean",
-    },
-    {
-      target: '0x20b3efe3ee5116904f4e08ca391715c5bf6703f630b6e91677010e308aebfc1',
-      method: "revive",
-    },
-  ],
+  policies,
   rpc: "https://api.cartridge.gg/x/babybeast-slot/katana",
 }) as never as Connector;
 
