fcf40d5999254ad0f054dc742e57928b79be0c6e
RolandoDrRobot <rolando.arguello.email@gmail.com>
2024-11-07 20:31:45 -0600

    account

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 // import { useEffect, useMemo, useState } from "react";
 import { SDK, createDojoStore } from "@dojoengine/sdk";
 // import { getEntityIdFromKeys } from "@dojoengine/utils";
